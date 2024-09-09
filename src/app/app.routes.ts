aa14781dd3fecacb9de80f5a0143fb4c7061e82f
AliIsmail10 <ali.mohamed.ali31199@gmail.com>
2024-09-10 01:06:40 +0300

    Updated navbar and fixed paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,10 +32,3 @@ export const routes: Routes = [
     
 ];
 
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
-  })
-  export class AppRoutingModule { }
